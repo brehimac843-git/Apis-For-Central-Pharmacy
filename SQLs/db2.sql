cde14bac40b6ffb085cfb5a1ce020c6bbdbc7e09
Brehima <brehimac843@gmail.com>
2026-06-16 15:26:58 +0000

    add of sqls data

---
+++ SQLs/db2.sql
@@ -0,0 +1,434 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict bZJae7gtwfCKOGfLG1g3SsAiW0r7numqhcNvKycbCejsb3F5SydcAbvAJIuABp0
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching based on trigrams';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: amo_drugs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.amo_drugs (
+    id integer NOT NULL,
+    drug_id integer,
+    reimbursement_rate integer
+);
+
+
+ALTER TABLE public.amo_drugs OWNER TO postgres;
+
+--
+-- Name: amo_drugs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.amo_drugs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.amo_drugs_id_seq OWNER TO postgres;
+
+--
+-- Name: amo_drugs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.amo_drugs_id_seq OWNED BY public.amo_drugs.id;
+
+
+--
+-- Name: drugs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.drugs (
+    id integer NOT NULL,
+    name character varying(150) NOT NULL,
+    dosage character varying(50),
+    form character varying(50),
+    buying_price numeric(10,2),
+    selling_price numeric(10,2),
+    stock_quantity integer DEFAULT 0,
+    expiry_date date
+);
+
+
+ALTER TABLE public.drugs OWNER TO postgres;
+
+--
+-- Name: drugs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.drugs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.drugs_id_seq OWNER TO postgres;
+
+--
+-- Name: drugs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.drugs_id_seq OWNED BY public.drugs.id;
+
+
+--
+-- Name: patients; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.patients (
+    id integer NOT NULL,
+    name character varying(150) NOT NULL,
+    phone character varying(30),
+    email character varying(150)
+);
+
+
+ALTER TABLE public.patients OWNER TO postgres;
+
+--
+-- Name: patients_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.patients_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.patients_id_seq OWNER TO postgres;
+
+--
+-- Name: patients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.patients_id_seq OWNED BY public.patients.id;
+
+
+--
+-- Name: sale_items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sale_items (
+    id integer NOT NULL,
+    sale_id integer,
+    drug_id integer,
+    quantity integer NOT NULL,
+    unit_price numeric(10,2) NOT NULL
+);
+
+
+ALTER TABLE public.sale_items OWNER TO postgres;
+
+--
+-- Name: sale_items_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.sale_items_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.sale_items_id_seq OWNER TO postgres;
+
+--
+-- Name: sale_items_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.sale_items_id_seq OWNED BY public.sale_items.id;
+
+
+--
+-- Name: sales; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sales (
+    id integer NOT NULL,
+    patient_id integer,
+    total_amount numeric(10,2),
+    payment_method character varying(50),
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.sales OWNER TO postgres;
+
+--
+-- Name: sales_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.sales_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.sales_id_seq OWNER TO postgres;
+
+--
+-- Name: sales_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.sales_id_seq OWNED BY public.sales.id;
+
+
+--
+-- Name: amo_drugs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.amo_drugs ALTER COLUMN id SET DEFAULT nextval('public.amo_drugs_id_seq'::regclass);
+
+
+--
+-- Name: drugs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.drugs ALTER COLUMN id SET DEFAULT nextval('public.drugs_id_seq'::regclass);
+
+
+--
+-- Name: patients id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.patients ALTER COLUMN id SET DEFAULT nextval('public.patients_id_seq'::regclass);
+
+
+--
+-- Name: sale_items id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sale_items ALTER COLUMN id SET DEFAULT nextval('public.sale_items_id_seq'::regclass);
+
+
+--
+-- Name: sales id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sales ALTER COLUMN id SET DEFAULT nextval('public.sales_id_seq'::regclass);
+
+
+--
+-- Data for Name: amo_drugs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.amo_drugs (id, drug_id, reimbursement_rate) FROM stdin;
+1	1	80
+2	2	70
+3	4	90
+\.
+
+
+--
+-- Data for Name: drugs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.drugs (id, name, dosage, form, buying_price, selling_price, stock_quantity, expiry_date) FROM stdin;
+1	Paracetamol	500mg	Tablet	100.00	150.00	150	2026-12-31
+2	Amoxicillin	250mg	Capsule	\N	300.00	120	2026-10-15
+3	Ibuprofen	400mg	Tablet	\N	250.00	90	2026-11-10
+4	Metformin	500mg	Tablet	\N	400.00	60	2027-01-05
+5	Artemether-Lumefantrine	20/120mg	Tablet	\N	1200.00	50	2026-08-15
+\.
+
+
+--
+-- Data for Name: patients; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.patients (id, name, phone, email) FROM stdin;
+\.
+
+
+--
+-- Data for Name: sale_items; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sale_items (id, sale_id, drug_id, quantity, unit_price) FROM stdin;
+\.
+
+
+--
+-- Data for Name: sales; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sales (id, patient_id, total_amount, payment_method, created_at) FROM stdin;
+\.
+
+
+--
+-- Name: amo_drugs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.amo_drugs_id_seq', 3, true);
+
+
+--
+-- Name: drugs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.drugs_id_seq', 5, true);
+
+
+--
+-- Name: patients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.patients_id_seq', 1, false);
+
+
+--
+-- Name: sale_items_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.sale_items_id_seq', 1, false);
+
+
+--
+-- Name: sales_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.sales_id_seq', 1, false);
+
+
+--
+-- Name: amo_drugs amo_drugs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.amo_drugs
+    ADD CONSTRAINT amo_drugs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: drugs drugs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.drugs
+    ADD CONSTRAINT drugs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: patients patients_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.patients
+    ADD CONSTRAINT patients_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sale_items sale_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sale_items
+    ADD CONSTRAINT sale_items_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sales sales_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sales
+    ADD CONSTRAINT sales_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: idx_drug_name; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_drug_name ON public.drugs USING btree (name);
+
+
+--
+-- Name: idx_drug_name_trgm; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_drug_name_trgm ON public.drugs USING gin (name public.gin_trgm_ops);
+
+
+--
+-- Name: amo_drugs amo_drugs_drug_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.amo_drugs
+    ADD CONSTRAINT amo_drugs_drug_id_fkey FOREIGN KEY (drug_id) REFERENCES public.drugs(id);
+
+
+--
+-- Name: sale_items sale_items_drug_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sale_items
+    ADD CONSTRAINT sale_items_drug_id_fkey FOREIGN KEY (drug_id) REFERENCES public.drugs(id);
+
+
+--
+-- Name: sale_items sale_items_sale_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sale_items
+    ADD CONSTRAINT sale_items_sale_id_fkey FOREIGN KEY (sale_id) REFERENCES public.sales(id);
+
+
+--
+-- Name: sales sales_patient_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sales
+    ADD CONSTRAINT sales_patient_id_fkey FOREIGN KEY (patient_id) REFERENCES public.patients(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict bZJae7gtwfCKOGfLG1g3SsAiW0r7numqhcNvKycbCejsb3F5SydcAbvAJIuABp0
+
